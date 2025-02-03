1824a5b3f0f90bf8c51b20515e7933a14b9dbd49
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-02-03 19:51:43 -0300

    Configurações iniciais do React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// CSS
 import "./App.css";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
     return (
         <div className="App">
-            <h1>MiniBlog</h1>
+            <BrowserRouter>
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
