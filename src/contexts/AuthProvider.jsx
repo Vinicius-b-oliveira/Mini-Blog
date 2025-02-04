6cdb8fc0b97f786676fb76811dabb75edc294c5b
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-02-04 15:18:26 -0300

    Criando a base do contexto de autenticação

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,7 @@
+import { AuthContext } from "./AuthContext";
+
+export const AuthProvider = ({ children, value }) => {
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
+};
