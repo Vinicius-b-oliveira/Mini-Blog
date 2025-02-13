9d0af02b91944aa2121cb8624e0a3fce1cdc780b
unknown <viniciusbuenodeoliveira2017@gmail.com>
2025-02-13 18:06:40 -0300

    Atualizando estilos da navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,49 +4,100 @@ import { useAuthValue } from "../../contexts/AuthContext";
 
 // CSS
 import styles from "./Navbar.module.css";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 const Navbar = () => {
     const { user } = useAuthValue();
     const { logout } = useAuthentication();
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className={styles.navbar}>
-            <Link to="/" className={styles.brand}>
-                Mini <span>Blog</span>
-            </Link>
-            <ul className={styles.links_list}>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                {!user && (
-                    <>
-                        <li>
-                            <NavLink to="/login">Entrar</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/register">Cadastrar</NavLink>
-                        </li>
-                    </>
-                )}
-                {user && (
-                    <>
-                        <li>
-                            <NavLink to="/posts/create">Novo post</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/dashboard">Dashboard</NavLink>
-                        </li>
-                    </>
-                )}
-                <li>
-                    <NavLink to="/about">Sobre</NavLink>
-                </li>
-                {user && (
+            <div className={styles.navContent}>
+                <Link to="/" className={styles.brand}>
+                    Mini <span>Blog</span>
+                </Link>
+
+                {/* Botão para abrir/fechar menu no mobile */}
+                <button
+                    className={styles.menuToggle}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    {menuOpen ? <X size={24} /> : <Menu size={24} />}
+                </button>
+
+                {/* Lista de links */}
+                <ul
+                    className={`${styles.links_list} ${menuOpen ? styles.open : ""}`}
+                >
                     <li>
-                        <button onClick={logout}>Sair</button>
+                        <NavLink to="/" onClick={() => setMenuOpen(false)}>
+                            Home
+                        </NavLink>
                     </li>
-                )}
-            </ul>
+                    {!user && (
+                        <>
+                            <li>
+                                <NavLink
+                                    to="/login"
+                                    onClick={() => setMenuOpen(false)}
+                                >
+                                    Entrar
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/register"
+                                    onClick={() => setMenuOpen(false)}
+                                >
+                                    Cadastrar
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+                    {user && (
+                        <>
+                            <li>
+                                <NavLink
+                                    to="/posts/create"
+                                    onClick={() => setMenuOpen(false)}
+                                >
+                                    Novo post
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/dashboard"
+                                    onClick={() => setMenuOpen(false)}
+                                >
+                                    Dashboard
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/about"
+                                    onClick={() => setMenuOpen(false)}
+                                >
+                                    Sobre
+                                </NavLink>
+                            </li>
+                            <li>
+                                <button
+                                    className={styles.logoutButton}
+                                    onClick={() => {
+                                        logout();
+                                        setMenuOpen(false);
+                                    }}
+                                >
+                                    Sair
+                                </button>
+                            </li>
+                        </>
+                    )}
+                </ul>
+            </div>
         </nav>
     );
 };
